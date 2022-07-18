d2e5f6b620b85ba0b2f8bd271ffe86efe200398f
linetlucy-genchabe <linetlucy21@gmail.com>
2022-07-18 17:12:49 +0300

    add app url paths

--- bounty/urls.py
+++ bounty/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url, include
+from django.urls import  re_path as url, include,path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -6,10 +6,20 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.index, name= 'index'),
-    # url(r'register/$',views.register ),
-    # url(r'accounts/login/$',views.user_login, name='login'),
-    # url(r'logout/$',views.signout),
-    # url(r'^accounts/profile/$', views.user_profiles, name='profile'),
+    url(r'register/$',views.register ),
+    url(r'accounts/login/$',views.user_login, name='login'),
+    url(r'logout/$',views.signout),
+    url(r'^accounts/profile/$', views.user_profiles, name='profile'),
+    url(r'^new/post$', views.new_post, name='new-post'),
+    url(r'^single/post/(\d+)$', views.single_post, name='single_post'),
+    url(r'^search/', views.search_posts, name='search_results'),
+    
+
+    path('contact',views.contact, name = 'contact'),
+    path('cart',views.cart, name ='cart'),
+    path('updatecart',views.updateCart),
+    path('updatequantity',views.updateQuantity),
+    path('checkout', views.checkout, name = 'checkout'),
   
    
    
