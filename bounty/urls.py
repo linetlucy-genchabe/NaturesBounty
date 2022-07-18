6c8cb27384688855f5178a223986ccbd42cccdc8
linetlucy-genchabe <linetlucy21@gmail.com>
2022-07-18 14:24:03 +0300

    added url patterns

---
+++ bounty/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import url, include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    url(r'^$', views.index, name= 'index'),
+    # url(r'register/$',views.register ),
+    # url(r'accounts/login/$',views.user_login, name='login'),
+    # url(r'logout/$',views.signout),
+    # url(r'^accounts/profile/$', views.user_profiles, name='profile'),
+  
+   
+   
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
